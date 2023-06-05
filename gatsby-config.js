2b8fa593b2baf869afd5468b8ceabe64e90cc64a
Shubham Khandelwal <shubham.khandelwal@shubham.khandelwal-TJLJTCD9J0>
2023-06-05 13:28:25 +0530

    completed the tutorials

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,20 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `My Gatsby Site: first`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
